c6b93ce8d218e65ac6ef859c573e3c8c0a9126b7
Kevin Wang <kevin.wang@fugle.tw>
2024-01-08 00:53:42 +0800

    feat: support for fetching futures institutional investors' trades

--- src/scrapers/taifex-scraper.ts
+++ src/scrapers/taifex-scraper.ts
@@ -154,13 +154,13 @@ export class TaifexScraper extends Scraper {
     return data.filter(row => afterhours ? row.session === '盤後' : row.session === '一般');
   }
 
-  async fetchTxfInstTrades(options: { date: string }) {
-    const { date } = options;
+  async fetchFuturesInstTrades(options: { date: string, symbol: string }) {
+    const { date, symbol } = options;
     const queryDate = DateTime.fromISO(date).toFormat('yyyy/MM/dd');
     const form = new URLSearchParams({
       queryStartDate: queryDate,
       queryEndDate: queryDate,
-      commodityId: 'TXF',
+      commodityId: symbol,
     });
     const url = 'https://www.taifex.com.tw/cht/3/futContractsDateDown';
     const response = await this.httpService.post(url, form, { responseType: 'arraybuffer' });
@@ -172,7 +172,7 @@ export class TaifexScraper extends Scraper {
 
     const data: Record<string, any> = {};
     data.date = date,
-    data.symbol = FutOpt.TXF,
+    data.symbol = symbol,
     data.name = fini[1],
     data.finiLongTradeVolume = numeral(fini[3]).value();
     data.finiLongTradeValue = numeral(fini[4]).value();
@@ -213,63 +213,14 @@ export class TaifexScraper extends Scraper {
     return data;
   }
 
+  async fetchTxfInstTrades(options: { date: string }) {
+    const { date } = options;
+    return this.fetchFuturesInstTrades({ date, symbol: 'TXF' })
+  }
+
   async fetchMxfInstTrades(options: { date: string }) {
     const { date } = options;
-    const queryDate = DateTime.fromISO(date).toFormat('yyyy/MM/dd');
-    const form = new URLSearchParams({
-      queryStartDate: queryDate,
-      queryEndDate: queryDate,
-      commodityId: 'MXF',
-    });
-    const url = 'https://www.taifex.com.tw/cht/3/futContractsDateDown';
-    const response = await this.httpService.post(url, form, { responseType: 'arraybuffer' });
-    if (response.data.toString().includes('查無資料')) return null;
-
-    const csv = iconv.decode(response.data, 'big5');
-    const json = await csvtojson({ noheader: true, output: 'csv' }).fromString(csv);
-    const [_, dealers, sitc, fini] = json;
-
-    const data: Record<string, any> = {};
-    data.date = date,
-    data.symbol = FutOpt.MXF,
-    data.name = fini[1],
-    data.finiLongTradeVolume = numeral(fini[3]).value();
-    data.finiLongTradeValue = numeral(fini[4]).value();
-    data.finiShortTradeVolume = numeral(fini[5]).value();
-    data.finiShortTradeValue = numeral(fini[6]).value();
-    data.finiNetTradeVolume = numeral(fini[7]).value();
-    data.finiNetTradeValue = numeral(fini[8]).value();
-    data.finiLongOiVolume = numeral(fini[9]).value();
-    data.finiLongOiValue = numeral(fini[10]).value();
-    data.finiShortOiVolume = numeral(fini[11]).value();
-    data.finiShortOiValue = numeral(fini[12]).value();
-    data.finiNetOiVolume = numeral(fini[13]).value();
-    data.finiNetOiValue = numeral(fini[14]).value();
-    data.sitcLongTradeVolume = numeral(sitc[3]).value();
-    data.sitcLongTradeValue = numeral(sitc[4]).value();
-    data.sitcShortTradeVolume = numeral(sitc[5]).value();
-    data.sitcShortTradeValue = numeral(sitc[6]).value();
-    data.sitcNetTradeVolume = numeral(sitc[7]).value();
-    data.sitcNetTradeValue = numeral(sitc[8]).value();
-    data.sitcLongOiVolume = numeral(sitc[9]).value();
-    data.sitcLongOiValue = numeral(sitc[10]).value();
-    data.sitcShortOiVolume = numeral(sitc[11]).value();
-    data.sitcShortOiValue = numeral(sitc[12]).value();
-    data.sitcNetOiVolume = numeral(sitc[13]).value();
-    data.sitcNetOiValue = numeral(sitc[14]).value();
-    data.dealersLongTradeVolume = numeral(dealers[3]).value();
-    data.dealersLongTradeValue = numeral(dealers[4]).value();
-    data.dealersShortTradeVolume = numeral(dealers[5]).value();
-    data.dealersShortTradeValue = numeral(dealers[6]).value();
-    data.dealersNetTradeVolume = numeral(dealers[7]).value();
-    data.dealersNetTradeValue = numeral(dealers[8]).value();
-    data.dealersLongOiVolume = numeral(dealers[9]).value();
-    data.dealersLongOiValue = numeral(dealers[10]).value();
-    data.dealersShortOiVolume = numeral(dealers[11]).value();
-    data.dealersShortOiValue = numeral(dealers[12]).value();
-    data.dealersNetOiVolume = numeral(dealers[13]).value();
-    data.dealersNetOiValue = numeral(dealers[14]).value();
-    return data;
+    return this.fetchFuturesInstTrades({ date, symbol: 'MXF' })
   }
 
   async fetchTxoInstTrades(options: { date: string }) {
--- test/scrapers/taifex-scraper.spec.ts
+++ test/scrapers/taifex-scraper.spec.ts
@@ -130,6 +130,77 @@ describe('TaifexScraper', () => {
     });
   });
 
+  describe('.fetchFuturesInstTrades()', () => {
+    it('should fetch TXF institutional investors\' trades for the given date', async () => {
+      mockAxios.post.mockResolvedValueOnce({ data: fs.readFileSync('./test/fixtures/taifex-futures-inst-trades.csv') });
+
+      const data = await scraper.fetchFuturesInstTrades({ date: '2023-01-30', symbol: 'TXF' });
+      const url = 'https://www.taifex.com.tw/cht/3/futContractsDateDown';
+      const form = new URLSearchParams({
+        queryStartDate: '2023/01/30',
+        queryEndDate: '2023/01/30',
+        commodityId: 'TXF',
+      });
+      expect(mockAxios.post).toHaveBeenCalledWith(url, form, { responseType: 'arraybuffer' });
+      expect(data).toBeDefined();
+      expect(data).toEqual({
+        date: '2023-01-30',
+        symbol: 'TXF',
+        name: '臺股期貨',
+        finiLongTradeVolume: 61232,
+        finiLongTradeValue: 187462698,
+        finiShortTradeVolume: 60146,
+        finiShortTradeValue: 184303292,
+        finiNetTradeVolume: 1086,
+        finiNetTradeValue: 3159406,
+        finiLongOiVolume: 32100,
+        finiLongOiValue: 99233073,
+        finiShortOiVolume: 24001,
+        finiShortOiValue: 74192341,
+        finiNetOiVolume: 8099,
+        finiNetOiValue: 25040732,
+        sitcLongTradeVolume: 2237,
+        sitcLongTradeValue: 6907887,
+        sitcShortTradeVolume: 449,
+        sitcShortTradeValue: 1384268,
+        sitcNetTradeVolume: 1788,
+        sitcNetTradeValue: 5523619,
+        sitcLongOiVolume: 10112,
+        sitcLongOiValue: 31260237,
+        sitcShortOiVolume: 15995,
+        sitcShortOiValue: 49446943,
+        sitcNetOiVolume: -5883,
+        sitcNetOiValue: -18186706,
+        dealersLongTradeVolume: 14205,
+        dealersLongTradeValue: 43588157,
+        dealersShortTradeVolume: 17049,
+        dealersShortTradeValue: 52346096,
+        dealersNetTradeVolume: -2844,
+        dealersNetTradeValue: -8757939,
+        dealersLongOiVolume: 10822,
+        dealersLongOiValue: 33446397,
+        dealersShortOiVolume: 5797,
+        dealersShortOiValue: 17917728,
+        dealersNetOiVolume: 5025,
+        dealersNetOiValue: 15528669,
+      });
+    });
+
+    it('should return null when no data is available', async () => {
+      mockAxios.post.mockResolvedValueOnce({ data: fs.readFileSync('./test/fixtures/taifex-futures-inst-trades-no-data.html') });
+
+      const data = await scraper.fetchFuturesInstTrades({ date: '2023-01-01', symbol: 'TXF' });
+      const url = 'https://www.taifex.com.tw/cht/3/futContractsDateDown';
+      const form = new URLSearchParams({
+        queryStartDate: '2023/01/01',
+        queryEndDate: '2023/01/01',
+        commodityId: 'TXF',
+      });
+      expect(mockAxios.post).toHaveBeenCalledWith(url, form, { responseType: 'arraybuffer' });
+      expect(data).toBe(null);
+    });
+  });
+
   describe('.fetchTxfInstTrades()', () => {
     it('should fetch TXF institutional investors\' trades for the given date', async () => {
       mockAxios.post.mockResolvedValueOnce({ data: fs.readFileSync('./test/fixtures/txf-inst-trades.csv') });
